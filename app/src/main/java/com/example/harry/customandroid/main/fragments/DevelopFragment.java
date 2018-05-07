beb96ddd0f5289d4f0ba84b67a6221126163964d
RedColinar <1126525692@qq.com>
2018-05-07 14:40:09 +0800

    add timeline

--- app/src/main/java/com/example/harry/customandroid/main/fragments/DevelopFragment.java
+++ app/src/main/java/com/example/harry/customandroid/main/fragments/DevelopFragment.java
@@ -5,6 +5,7 @@ import android.view.View;
 import com.example.harry.customandroid.R;
 import com.example.harry.customandroid.tabs.develop.alarm.AlarmActivity;
 import com.example.harry.customandroid.base.BaseFragment;
+import com.example.harry.customandroid.tabs.develop.timeline.TimelineActivity;
 
 import butterknife.OnClick;
 
@@ -19,11 +20,17 @@ public class DevelopFragment extends BaseFragment {
         return R.layout.fragment_main_develop;
     }
 
-    @OnClick({R.id.main_develop_alarm})
+    @OnClick({
+            R.id.main_develop_alarm,
+            R.id.main_develop_timeline
+    })
     void onClick(View view) {
         switch (view.getId()) {
             case R.id.main_develop_alarm:
                 start(AlarmActivity.class);
+                break;
+            case R.id.main_develop_timeline:
+                start(TimelineActivity.class);
                 break;
         }
     }
---
+++ app/src/main/java/com/example/harry/customandroid/tabs/develop/timeline/TimelineActivity.java
@@ -0,0 +1,16 @@
+package com.example.harry.customandroid.tabs.develop.timeline;
+
+import com.example.harry.customandroid.R;
+import com.example.harry.customandroid.base.BaseActivity;
+
+public class TimelineActivity extends BaseActivity {
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_timeline;
+    }
+
+    @Override
+    public int getTitleId() {
+        return R.string.timeline_title;
+    }
+}
