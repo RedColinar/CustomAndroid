6377226e671ee87290b8d54f893a41c81bd62be4
RedColinar <1126525692@qq.com>
2018-09-17 20:59:03 +0800

    gradle 插件 自动加固

---
+++ buildSrc/src/main/groovy/com/pq/pqplugin/PackageExtension.groovy
@@ -0,0 +1,5 @@
+package com.pq.pqplugin
+
+class PackageExtension {
+    def appVersion
+}
---
+++ pqplugin/src/main/groovy/com.example.pqplugin/PqPlugin.groovy
@@ -0,0 +1,16 @@
+package com.example.pqplugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class PqPlugin implements Plugin<Project>{
+
+    @Override
+    void apply(Project project) {
+        println "-----------this is pq plugin!----------"
+        println "-----------this is pq plugin!----------"
+        println "-----------this is pq plugin!----------"
+        println "-----------this is pq plugin!----------"
+        println "-----------this is pq plugin!----------"
+    }
+}
