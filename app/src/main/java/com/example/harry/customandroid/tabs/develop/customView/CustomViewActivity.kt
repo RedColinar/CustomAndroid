9f747af5e24105539eb04489ae806e310c08eb62
panqiang <1126525692@qq.com>
2019-04-09 20:29:45 +0800

    dash

--- app/src/main/java/com/example/harry/customandroid/tabs/develop/customView/CustomViewActivity.kt
+++ app/src/main/java/com/example/harry/customandroid/tabs/develop/customView/CustomViewActivity.kt
@@ -2,13 +2,32 @@ package com.example.harry.customandroid.tabs.develop.customView
 
 import com.example.harry.customandroid.R
 import com.example.harry.customandroid.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_custom_view.*
 
 class CustomViewActivity : BaseActivity() {
+
     override fun getLayoutId(): Int {
         return R.layout.activity_custom_view
     }
 
     override fun getTitleId(): Int {
         return R.string.custom_view_title
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+//        var index = 0
+//        val timer = Timer()
+//        val timerTask = object : TimerTask() {
+//            override fun run() {
+//                dash_view.pointTo(index)
+//                index++
+//                index %= DashView.STEP + 1
+//            }
+//        }
+//
+//        timer.schedule(timerTask, 1000, 1000)
+        dash_view.pointTo(0)
     }
 }
--- app/src/main/java/com/example/harry/customandroid/tabs/develop/customView/DashView.kt
+++ app/src/main/java/com/example/harry/customandroid/tabs/develop/customView/DashView.kt
@@ -18,24 +18,29 @@ class DashView @JvmOverloads constructor(context: Context?, attrs: AttributeSet?
 
     companion object {
         const val ANGLE: Float = 120f
-        val PADDING: Float = dp2px(10f)
-        val STROKE_WIDTH = dp2px(8f)
-        val STROKE_LENGTH = dp2px(10f)
-        val POINTER_LENGTH = dp2px(110f)
         const val STEP = 10
+        const val START_ANGLE = 90 + ANGLE / 2
+        const val ANGLE_PER_STEP = (360 - ANGLE) / STEP
+
+        val PADDING: Float = dp2px(10f)
+        val STROKE_WIDTH = dp2px(1f)
+        val STROKE_LENGTH = dp2px(10f)
+        val POINTER_LENGTH = dp2px(210f)
     }
 
     init {
         paint.isAntiAlias = true
         paint.color = Color.BLACK
-        paint.strokeCap = Paint.Cap.ROUND
         paint.strokeWidth = STROKE_WIDTH
         paint.style = Paint.Style.STROKE
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
-        path.addArc(dp2px(PADDING), dp2px(PADDING), w - dp2px(PADDING), h - dp2px(PADDING), 90 + ANGLE / 2, 360 - ANGLE)
+
+        val r = Math.min(w / 2 - dp2px(PADDING), h / 2 - dp2px(PADDING))
+        val rect = RectF(w / 2f - r, h / 2f - r, w / 2f + r, h / 2f + r)
+        path.addArc(rect, START_ANGLE, 360 - ANGLE)
 
         val pathMeasure = PathMeasure(path, false)
         val advance = (pathMeasure.length - STROKE_WIDTH) / STEP
@@ -44,7 +49,7 @@ class DashView @JvmOverloads constructor(context: Context?, attrs: AttributeSet?
         effectPath.addRect(0f, 0f, STROKE_WIDTH, STROKE_LENGTH, Path.Direction.CW)
         effect = PathDashPathEffect(effectPath, advance, 0f, PathDashPathEffect.Style.MORPH)
 
-        point(0)
+        pointTo(0)
     }
 
     override fun onDraw(canvas: Canvas?) {
@@ -58,14 +63,15 @@ class DashView @JvmOverloads constructor(context: Context?, attrs: AttributeSet?
         canvas?.drawPath(pointerPath, paint)
     }
 
-    fun point(index: Int) {
+    fun pointTo(index: Int) {
         val cx: Float = width / 2f
         val cy: Float = height / 2f
 
-        val radius = Math.toRadians((90 + ANGLE / 2 + index * (360 - ANGLE) / STEP).toDouble())
+        val radius = Math.toRadians((START_ANGLE + index * ANGLE_PER_STEP).toDouble())
         val cosA: Double = Math.cos(radius)
         val sinA: Double = Math.sin(radius)
 
+        pointerPath.reset()
         pointerPath.moveTo(cx, cy)
         pointerPath.lineTo(
                 cx + POINTER_LENGTH * cosA.toFloat(),
